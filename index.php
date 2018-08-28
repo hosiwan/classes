121e0fc21f27e1283d36730292edbe31f7b94ec2
Salomao Batista <salomaum.batiata@gmail.com>
2018-08-27 21:09:30 -0300

    segundo commit

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-echo "teste";
+<?php
+require_once "pessoa.php";
+$pessoa = new Pessoa;
+$pessoa->andar();
+
+?>
---
+++ pessoa.php
@@ -0,0 +1,14 @@
+<?php
+Class Pessoa {
+	protected $altura;
+	protected $nome;
+
+	public function andar(){
+	echo "Andando...\n";
+}
+
+}
+
+
+
+?>
